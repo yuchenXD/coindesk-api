60e1a398f67ba16528e6c7133dda355be7d9c9ba
adam.liu <adam.liu@imsoft.com.tw>
2025-04-20 16:56:40 +0800

    feat: logger
    新增項目：
    log 回傳打印

--- src/main/java/com/coindesk/aop/LoggingAspect.java
+++ src/main/java/com/coindesk/aop/LoggingAspect.java
@@ -3,9 +3,11 @@ package com.coindesk.aop;
 import javax.servlet.http.HttpServletRequest;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -64,6 +66,39 @@ public class LoggingAspect {
         appendLine(sb, "    args:        : {}");
         appendLine(sb, LINE);
         log.info(sb.toString(), method, url, contentType, className, methodName, args);
+    }
+
+    /**
+     * 在方法執行後記錄返回值
+     * 
+     * @param joinPoint 連接點
+     * @param result 返回值
+     */
+    @AfterReturning(pointcut = "controllerPointcut()", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        // log
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        // http
+        int status = 0;
+        if (result instanceof ResponseEntity<?>) {
+            status = ((ResponseEntity<?>) result).getStatusCodeValue();
+        } 
+        // controller
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        // log
+        appendLine(sb, LINE);
+        // http
+        appendLine(sb, "  http");
+        appendLine(sb, "    status       : {}");
+        appendLine(sb, "  controller");
+        appendLine(sb, "    class        : {}() { }");
+        appendLine(sb, "    method       : {}()");
+        appendLine(sb, "  return");
+        appendLine(sb, "    result       : {}");
+        appendLine(sb, LINE);
+        log.info(sb.toString(),status, className, methodName, result);
     }
 
     /**
--- src/main/java/com/coindesk/exception/GlobalExceptionHandler.java
+++ src/main/java/com/coindesk/exception/GlobalExceptionHandler.java
@@ -16,64 +16,120 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * @category Exception
- * @author AI Assistant
+ * @author Yuchen Liu
  * @description 全局異常處理器
  */
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final static String LINE = "-----------------------------------------------------------";
+
     /**
      * 處理實體未找到異常
-     * 
+     *
      * @param e 異常
      * @return ResponseEntity
      */
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<BaseResponse<String>> handleEntityNotFoundException(EntityNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        // 創建響應
+        ResponseEntity<BaseResponse<String>> response = ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new BaseResponse<>(StatusCode.DATA_NOT_FOUND));
+
+        // 記錄日誌
+        logger(e, response);
+
+        return response;
     }
 
     /**
      * 處理參數驗證異常
-     * 
+     *
      * @param e 異常
      * @return ResponseEntity
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<BaseResponse<String>> handleMethodArgumentNotValidException(
             MethodArgumentNotValidException e) {
+        // 構建錯誤消息
         StringBuilder errorMsg = new StringBuilder();
         for (FieldError error : e.getBindingResult().getFieldErrors()) {
             errorMsg.append(error.getField()).append(": ").append(error.getDefaultMessage()).append("; ");
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+
+        // 創建響應
+        ResponseEntity<BaseResponse<String>> response = ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new BaseResponse<>(StatusCode.INVALID_REQUEST));
+
+        // 記錄日誌
+        logger(e, response);
+
+        return response;
     }
 
     /**
      * 處理非法參數異常
-     * 
+     *
      * @param e 異常
      * @return ResponseEntity
      */
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<BaseResponse<String>> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new BaseResponse<>(StatusCode.INVALID_REQUEST));
+        // 創建響應
+        ResponseEntity<BaseResponse<String>> response = ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new BaseResponse<>(StatusCode.DUPLICATE_DATA));
+
+        // 記錄日誌
+        logger(e, response);
+
+        return response;
     }
 
     /**
      * 處理其他異常
-     * 
+     *
      * @param e 異常
      * @return ResponseEntity
      */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<BaseResponse<String>> handleException(Exception e) {
-        log.error("系統異常: ", e);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        // 創建響應
+        ResponseEntity<BaseResponse<String>> response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new BaseResponse<>(StatusCode.FAILURE));
+
+        // 記錄日誌
+        logger(e, response);
+
+        return response;
+    }
+
+    private void logger(Exception e, ResponseEntity<BaseResponse<String>> response) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        appendLine(sb, LINE);
+        appendLine(sb, "  exception");
+        appendLine(sb, "    type         : {}");
+        appendLine(sb, "    message      : {}");
+        appendLine(sb, "  http");
+        appendLine(sb, "    status       : {}");
+        appendLine(sb, "  response");
+        appendLine(sb, "    body         : {}");
+        appendLine(sb, LINE);
+        log.error(sb.toString(),
+                e.getClass().getSimpleName(),
+                e.getMessage(),
+                response.getStatusCodeValue(),
+                response.getBody());
+    }
+
+    /**
+     * StringBuilder 自動換行
+     *
+     * @param sb   StringBuilder
+     * @param text 訊息
+     */
+    private void appendLine(StringBuilder sb, String text) {
+        sb.append(text).append("\n");
     }
 }
